734cf25efe9ab31596dbf3cfdcc4dbaea42a271c
Vasc01 <vasil.andreev.workspace@gmail.com>
2023-12-22 17:16:28 +0200

    (#3) - LZW compression size reduced with variable bit sequences.

--- core/compression.py
+++ core/compression.py
@@ -63,9 +63,86 @@ class LZWCompression(CompressionABC):
         print(temporary_data_output)
 
         # integer array to binary
-        compressed_data = struct.pack('>' + 'i' * len(temporary_data_output), *temporary_data_output)
+        # compressed_data = struct.pack('>' + 'i' * len(temporary_data_output), *temporary_data_output)
+        compressed_data = self.pack(temporary_data_output)
 
         return compressed_data
+
+    def pack(self, codepoints):
+        """Variable bit lengths"""
+        tailbits = []
+        codesize = 4096
+
+        minwidth = 8
+        while (1 << minwidth) < codesize:
+            minwidth = minwidth + 1
+
+        nextwidth = minwidth
+
+        for pt in codepoints:
+
+            newbits = self.inttobits(pt, nextwidth)
+            tailbits = tailbits + newbits
+
+            # PAY ATTENTION. This calculation should be driven by the
+            # size of the upstream codebook, right now we're just trusting
+            # that everybody intends to follow the TIFF spec.
+            codesize = codesize + 1
+
+            if codesize >= (2 ** nextwidth):
+                nextwidth = nextwidth + 1
+
+            while len(tailbits) > 8:
+                nextbits = tailbits[:8]
+                nextbytes = self.bitstobytes(nextbits)
+                for bt in nextbytes:
+                    yield struct.pack("B", bt)
+
+                tailbits = tailbits[8:]
+
+        if tailbits:
+            tail = self.bitstobytes(tailbits)
+            for bt in tail:
+                yield struct.pack("B", bt)
+
+
+    def inttobits(self, anint, width=None):
+
+        remains = anint
+        retreverse = []
+        while remains:
+            retreverse.append(remains & 1)
+            remains = remains >> 1
+
+        retreverse.reverse()
+
+        ret = retreverse
+        if None != width:
+            ret_head = [0] * (width - len(ret))
+            ret = ret_head + ret
+
+        return ret
+
+    def bitstobytes(self, bits):
+
+        ret = []
+        nextbyte = 0
+        nextbit = 7
+        for bit in bits:
+            if bit:
+                nextbyte = nextbyte | (1 << nextbit)
+
+            if nextbit:
+                nextbit = nextbit - 1
+            else:
+                ret.append(nextbyte)
+                nextbit = 7
+                nextbyte = 0
+
+        if nextbit < 7: ret.append(nextbyte)
+        return ret
+
+
 
     def decode(self):
         raise NotImplemented
--- test/lzw_test.py
+++ test/lzw_test.py
@@ -2,7 +2,7 @@ import os
 
 from core.compression import LZWCompression
 
-in_file_path = r"C:\Workspace\1_PyCh_projects\file-compression-exercise\test\example_files\repeated_string_1"
+in_file_path = r"C:\Workspace\1_PyCh_projects\file-compression-exercise\test\example_files\treasure_island_ch1"
 
 in_file_size = os.stat(in_file_path).st_size
 print("Input file size:", in_file_size, "bytes")
@@ -15,11 +15,14 @@ lzw_compression = LZWCompression()
 compressed_data = lzw_compression.encode(uncompressed_data)
 print("Output data type:", type(compressed_data))
 
-out_file = open(r"C:\Workspace\1_PyCh_projects\file-compression-exercise\test\example_files\repeated_string_encoded",
-                "wb")
-out_file.write(compressed_data)
+out_file = (
+    open(r"C:\Workspace\1_PyCh_projects\file-compression-exercise\test\example_files\treasure_island_ch1_encoded",
+         "wb")
+)
+for data in compressed_data:
+    out_file.write(data)
 out_file.close()
 
-out_file_path = r"C:\Workspace\1_PyCh_projects\file-compression-exercise\test\example_files\repeated_string_encoded"
+out_file_path = r"C:\Workspace\1_PyCh_projects\file-compression-exercise\test\example_files\treasure_island_ch1_encoded"
 out_file_size = os.stat(out_file_path).st_size
 print("Output file size:", out_file_size, "bytes")
