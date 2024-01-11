fe1e9645ae5c80acf3555ec3814cb6661552a5f4
Vasc01 <vasil.andreev.workspace@gmail.com>
2024-01-11 17:52:35 +0200

    (#13) - File handler works with encoding and decoding. New files are created in both directions.

--- data/file_handler.py
+++ data/file_handler.py
@@ -2,8 +2,6 @@ import os.path
 import pickle
 from abc import ABC, abstractmethod
 
-from core.compression import LZWCompression, HuffmanCompression
-
 
 class FileHandlerABC(ABC):
 
@@ -14,76 +12,51 @@ class FileHandlerABC(ABC):
 
 class FileHandler(FileHandlerABC):
 
-    def __init__(self):
-        self.algorithm = None
-        self.file_path = None
-        self.file_name = None
-        self.file_extension = None
-        self.original_data = None
-        self.encoded_data = None
-        self.encoded_file_extension = None
-
-    def initiate_encode(self, complete_path, algorithm, file_name=None):
-
-        self.get_path_info(complete_path)
-        self.get_file_data(complete_path)
-        data_to_write = self.encode(algorithm)
-
-        if file_name:
-            self.file_name = file_name
-
-        self.write_in_file(data_to_write)
-
-    def get_path_info(self, complete_path):
+    @staticmethod
+    def get_path_info(complete_path):
         """Gets file location file name and extension from path.
 
         Args:
             complete_path (str): Expected to have path to file including file name and extension.
         """
-        self.file_path = os.path.split(complete_path)[0]
-        self.file_name = os.path.splitext(os.path.basename(complete_path))[0]
-        self.file_extension = os.path.splitext(complete_path)[1]
+        file_path = os.path.split(complete_path)[0]
+        file_name = os.path.splitext(os.path.basename(complete_path))[0]
+        file_extension = os.path.splitext(complete_path)[1]
 
-    def get_file_data(self, complete_path):
+        return file_path, file_name, file_extension
+
+    @staticmethod
+    def get_file_bytes(complete_path):
 
         with open(complete_path, 'rb') as f:
-            data_for_compression = f.read()
+            data = f.read()
 
-        self.original_data = data_for_compression
+        return data
 
-    def encode(self, algorithm):
+    @staticmethod
+    def write_in_file(data, file_path, file_name, file_extension):
 
-        if algorithm == "lzw":
-            encoder = LZWCompression()
-
-            encoded_data = encoder.encode(self.original_data)
-            bit_size = encoder.bit_size
-
-            self.encoded_file_extension = ".lzw"
-
-            return bit_size, encoded_data, self.file_extension
-
-        elif algorithm == "huf":
-            encoder = HuffmanCompression()
-
-            encoded_data = encoder.encode(self.original_data)
-            codebook = encoder.codebook
-
-            self.encoded_file_extension = ".huf"
-
-            return codebook, encoded_data, self.file_extension
-
-    def write_in_file(self, data_to_write):
-
-        new_path = os.path.join(self.file_path, (self.file_name + self.encoded_file_extension))
-        # new_path = self.file_path + self.file_name + self.encoded_file_extension
-
-        print(new_path)
+        # Names and locates the new file
+        new_path = os.path.join(file_path, (file_name + file_extension))
 
         with open(new_path, 'wb') as f:
-            pickle.dump(data_to_write, f)
+            pickle.dump(data, f)
 
-        print("New file created.")
+    @staticmethod
+    def read_from_file(file_path):
+
+        with open(file_path, 'rb') as f:
+            stored_data = pickle.load(f)
+
+        return stored_data
+
+    @staticmethod
+    def recreate_file(data, file_path, file_name, file_extension):
+
+        new_path = os.path.join(file_path, (file_name + file_extension))
+
+        with open(new_path, 'wb') as f:
+            f.write(data)
 
     def get_file_size(self, path):
         pass
