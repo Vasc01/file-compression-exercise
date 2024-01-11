ae79383aa6a1c92261eadc19b693b946e8a17337
Vasc01 <vasil.andreev.workspace@gmail.com>
2024-01-11 17:54:57 +0200

    (#15) - CLI functional. Rich module tested separately.

--- frontend/application.py
+++ frontend/application.py
@@ -1,23 +1,111 @@
 """ Demonstration of the backend capabilities.
 
-Makes use of fire CLI library
+Makes use of fire CLI library for input
+and rich library for output.
 """
 
-from core.compression import LZWCompression, HuffmanCompression
+from rich import print as rprint
 import fire
 
+from core.compression import LZWCompression, HuffmanCompression
 from data.file_handler import FileHandler
 
 
 class Application(object):
 
-    def encode(self, path: str, algorithm: str, new_name=None):
-        file_handler = FileHandler()
-        file_handler.initiate_encode(path, algorithm, new_name)
+    def __init__(self, file_handler, lzw_compression, huffman_compression):
+        self.file_handler = file_handler
+        self.lzw_compression = lzw_compression
+        self.huffman_compression = huffman_compression
 
-    def decode(self, file_to_decode: str, new_file_name: str):
-        print('application starts the decompress function')
+    def encode(self, complete_path: str, algorithm: str, new_name=None):
+        # complete path can be absolute path to file or just the file name
+        # files without extension are accepted as well
+
+        # path to file, file name, file extension
+        path_info = self.file_handler.get_path_info(complete_path)
+
+        file_path = path_info[0]
+        file_name = new_name if new_name else path_info[1]
+        file_extension = path_info[2]
+        file_extension_compressed = "." + algorithm
+
+        # file represented as bytes
+        data_for_compression = self.file_handler.get_file_bytes(complete_path)
+
+        # Holds compressed data and metadata.
+        compressed_data = self._execute_encoding_algorithm(algorithm, data_for_compression, file_extension)
+
+        # creates new file
+        self.file_handler.write_in_file(compressed_data,
+                                        file_path,
+                                        file_name,
+                                        file_extension_compressed)
+
+    def _execute_encoding_algorithm(self, algorithm, data_for_compression, file_extension):
+
+        if algorithm == "lzw":
+
+            encoded_data = self.lzw_compression.encode(data_for_compression)
+            bit_size = self.lzw_compression.bit_size
+
+            return bit_size, encoded_data, file_extension
+
+        elif algorithm == "huf":
+
+            encoded_data = self.huffman_compression.encode(data_for_compression)
+            codebook = self.huffman_compression.codebook
+
+            return codebook, encoded_data, file_extension
+
+    def decode(self, complete_path: str, new_name=None):
+
+        path_info = self.file_handler.get_path_info(complete_path)
+
+        file_path = path_info[0]
+        file_name = new_name if new_name else path_info[1]
+        file_extension = path_info[2]
+
+        retrieved_data = self.file_handler.read_from_file(complete_path)
+
+        file_extension_original = retrieved_data[2]
+
+        decompressed_data = self._execute_decoding_algorithm(retrieved_data, file_extension)
+
+        # recreates original file
+        self.file_handler.recreate_file(decompressed_data,
+                                        file_path,
+                                        file_name,
+                                        file_extension_original)
+
+    def _execute_decoding_algorithm(self, retrieved_data, file_extension):
+
+        if file_extension == ".lzw":
+
+            self.lzw_compression.bit_size = retrieved_data[0]
+            decoded_data = self.lzw_compression.decode(retrieved_data[1])
+
+            return decoded_data
+
+        elif file_extension == ".huf":
+
+            self.huffman_compression.codebook = retrieved_data[0]
+            decoded_data = self.huffman_compression.decode(retrieved_data[1])
+
+            return decoded_data
+
+    @staticmethod
+    def help():
+        rprint("[purple]This text is impossible to read")
+        rprint(":warning:")
 
 
 def run():
-    fire.Fire(Application)
+
+    file_handler = FileHandler()
+    lzw_compression = LZWCompression()
+    huffman_compression = HuffmanCompression()
+
+    fire.Fire(Application(file_handler=file_handler,
+                          lzw_compression=lzw_compression,
+                          huffman_compression=huffman_compression))
---
+++ sandbox/rich_experiment.py
@@ -0,0 +1,36 @@
+# Official rich documentation:
+# https://rich.readthedocs.io/en/latest/index.html
+
+from rich import print as rprint
+from rich.box import SIMPLE_HEAD
+from rich.table import Table
+from rich.console import Console
+table = Table(style="rgb(229,193,0)",
+              header_style="rgb(229,193,0)",
+              pad_edge=False,
+              show_edge=False,
+              collapse_padding=True,
+              box=SIMPLE_HEAD)
+console = Console()
+
+rprint()
+
+header = "LZW Compression"
+console.rule(f"[bold rgb(229,193,0)]{header}", style="rgb(229,193,0)")
+
+path = r"C:\Workspace\1_PyCh_projects\file-compression-exercise"
+
+rprint(f"[rgb(229,193,0)]path to containing folder: [green]{path}")
+
+table.add_column("", style="rgb(229,193,0)", no_wrap=True)
+table.add_column("name", style="green")
+table.add_column("size", justify="right", style="green")
+table.add_column("compression rate", style="rgb(23,255,23)")
+
+table.add_row("original", "test.txt", "350kB", "")
+table.add_row("converted", "test-test.lzw", "50kB", "3.55")
+
+rprint()
+console.print(table)
+
+rprint()
